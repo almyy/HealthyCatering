d73b97d05f191302b7348124106402745c3d6f4a
Martin <Martin@Martin-PC>
2013-03-05 15:07:12 +0100

    Initial commit

--- src/java/Beans/registerBean.java
+++ src/java/Beans/registerBean.java
@@ -8,34 +8,25 @@ import javax.faces.component.UIComponent;
 import javax.faces.component.UIInput;
 import javax.faces.context.FacesContext;
 import javax.faces.validator.Validator;
-import javax.faces.validator.ValidatorException;
 import javax.inject.Named;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
 
 @ManagedBean
 @SessionScoped
 @Named("Register")
-class registerBean implements Serializable, Validator {
+class registerBean implements Serializable {
     private String tempUsername;
     private String tempPassword;
     private String tempRepeatPass;
     private String tempAddress;
+    private int tempPostnumber;
+    private String tempCity;
     private String tempFirstName;
     private String tempSurname;
     private String tempEmail;
-    private int tempPhone;
+    private long tempPhone;
     
-    public void validatePasswords(FacesContext context, UIComponent component, Object value) {
-        String message = "";
-        String password = (String) value;
-        if(!tempPassword.equals(password)) {
-            ((UIInput)component).setValid(false);
-            message = "The passwords doesn't match, please retype";
-            context.addMessage(component.getClientId(context), new FacesMessage(message));
-        }
-        else {
-            tempRepeatPass = password;
-        }
-    }
     public void validateUsername(FacesContext context, UIComponent component, Object value) {
         String message = "";
         String username = (String) value;
@@ -44,10 +35,64 @@ class registerBean implements Serializable, Validator {
             message = "Enter a username";
             context.addMessage(component.getClientId(context), new FacesMessage(message));
         }
-        else {
-            tempUsername = username;
+    }
+    
+    public void validatePasswords(FacesContext context, UIComponent component, Object value) {
+        String message = "";
+        String password = (String) value;
+        boolean hasNumber = false;
+        for(int i = 0; i < password.length(); i++) {
+            if(Character.isDigit(password.charAt(i))) {
+                hasNumber = true;
+            }
+        }
+        if(!hasNumber && password.length() < 8) {
+            ((UIInput)component).setValid(false);
+            message = "You need to have at least one number and the password must be atleast 8 characters long";
+            context.addMessage(component.getClientId(context), new FacesMessage(message));
+        }
+        else tempPassword = password;
+    }
+    
+    public void validateRepPassword(FacesContext context, UIComponent component, Object value) {
+        String message = "";
+        String repPassword = (String) value;
+        if(!repPassword.equals(tempPassword)) {
+            ((UIInput)component).setValid(false);
+            message = "The passwords don't match.";
+            context.addMessage(component.getClientId(context), new FacesMessage(message));
         }
         
+    }
+    public void validate(FacesContext context, UIComponent component, Object value) {
+        String message = "";
+        String stringValue = (String) value;
+        if(stringValue == null || stringValue.equals("")) {
+            ((UIInput)component).setValid(false);
+            message = "The field cannot be empty";
+            context.addMessage(component.getClientId(context), new FacesMessage(message));
+        }
+    }
+    public void validatePostnumber(FacesContext context, UIComponent component, Object value) {
+        String message = "";
+        try {
+          Integer postnumber = (Integer) value;
+        } catch (Exception e) {
+            ((UIInput)component).setValid(false);
+            message = "The postnumber can only be numbers";
+            context.addMessage(component.getClientId(context), new FacesMessage(message));
+        }
+    }
+    public void validateEmail(FacesContext context, UIComponent component, Object value) {
+        String message = "";
+        try {
+            InternetAddress email = new InternetAddress((String) value);
+            email.validate();
+        } catch(AddressException ae) {
+            ((UIInput)component).setValid(false);
+            message = "Type a valid email address";
+            context.addMessage(component.getClientId(context), new FacesMessage(message));
+        }
     }
     public String getTempUsername() {
         return tempUsername;
@@ -91,15 +136,25 @@ class registerBean implements Serializable, Validator {
     public void setTempEmail(String tempEmail) {
         this.tempEmail = tempEmail;
     }
-    public int getTempPhone() {
+    public long getTempPhone() {
         return tempPhone;
     }
-    public void setTempPhone(int tempPhone) {
+    public void setTempPhone(long tempPhone) {
         this.tempPhone = tempPhone;
     }
+    public int getTempPostnumber() {
+        return tempPostnumber;
+    }
 
-    @Override
-    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void setTempPostnumber(int tempPostnumber) {
+        this.tempPostnumber = tempPostnumber;
+    }
+
+    public String getTempCity() {
+        return tempCity;
+    }
+
+    public void setTempCity(String tempCity) {
+        this.tempCity = tempCity;
     }
 }
