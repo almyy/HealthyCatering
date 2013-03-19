7f93f67f370224c8e704185eff126077c36df8b2
Jan Michael <Olsen>
2013-03-19 14:34:52 +0100

    ordre greier

--- src/java/DB/Database.java
+++ src/java/DB/Database.java
@@ -5,6 +5,7 @@ import logikk.Order;
 import java.sql.*;
 import java.util.ArrayList;
 import javax.annotation.Resource;
+import javax.faces.context.FacesContext;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
@@ -17,6 +18,7 @@ public class Database {
     @Resource(name = "jdbc/hc_realm")
     private DataSource ds;
     private Connection connection;
+    private String currentUser = FacesContext.getCurrentInstance().getExternalContext().getRemoteUser();
 
     public Database() {
         try {
@@ -54,29 +56,6 @@ public class Database {
         return orders;
     }
 
-  /*      public boolean logIn(User user) {
-        PreparedStatement sqlLogIn = null;
-        openConnection();
-        boolean ok = false;
-        try {
-            sqlLogIn = connection.prepareStatement("SELECT * FROM BRUKER WHERE BRUKERNAVN = '" + user.getUsername() + "' AND PASSORD = '" + user.getPassword() + "' ");
-            ResultSet res = sqlLogIn.executeQuery();
-            connection.commit();
-            if (res.next()) {
-                ok = true;
-            }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-            Cleaner.rollback(connection);
-
-        } finally {
-            Cleaner.setAutoCommit(connection);
-            Cleaner.closeSentence(sqlLogIn);
-        }
-        closeConnection();
-        return ok;
-    }
-    */
     //FOR ADMIN
     public void updateOrder(Order s){
         PreparedStatement sqlRead = null;
@@ -93,6 +72,7 @@ public class Database {
             Cleaner.closeSentence(sqlRead);
         }
     }
+    
     public ArrayList<Order> getOrderOverview() {
         ArrayList<Order> orders = new ArrayList();
         PreparedStatement sqlRead = null;
@@ -122,7 +102,6 @@ public class Database {
     }
     
     //FOR DRIVER
-
     public ArrayList<Order> getDriversList() {
         ArrayList<Order> orders = new ArrayList();
         PreparedStatement sqlRead = null;
@@ -151,9 +130,6 @@ public class Database {
         return orders;
 
     }
-
-
-    
 
     public boolean changePassword(User user) {
         PreparedStatement sqlLogIn = null;
@@ -192,7 +168,7 @@ public class Database {
             sqlRegNewuser.setString(3, user.getFirstName());
             sqlRegNewuser.setString(4, user.getSurname());
             sqlRegNewuser.setString(5, user.getAddress());
-            sqlRegNewuser.setString(6, user.getPhone());
+            sqlRegNewuser.setInt(6, user.getPhone());
             sqlRegNewuser.setInt(7, user.getPostnumber());
             sqlRegNewuser.executeUpdate();
 
@@ -214,7 +190,7 @@ public class Database {
         closeConnection();
         return ok;
     }
-    
+//FOR MENU
     public ArrayList<Dish> getDishes() {
         PreparedStatement sentence = null;
         openConnection();
@@ -226,7 +202,8 @@ public class Database {
             while (res.next()) {
                 int dishid = res.getInt("DISHID");
                 String dishname = res.getString("DISHNAME");
-                Dish newdish = new Dish(dishid, dishname, 0.0, 1);
+                double dishprice = res.getDouble("DISHPRICE");
+                Dish newdish = new Dish(dishid, dishname, dishprice, 1);
                 dishes.add(newdish);
             }
         } catch (SQLException e) {
@@ -240,6 +217,20 @@ public class Database {
         closeConnection();
         return dishes;
     }
+    
+//    public boolean order(ArrayList<Dish> orderList){
+//        PreparedStatement statement = null;
+//        openConnection();
+//        boolean result = true;
+//        try {
+//            connection.setAutoCommit(false);
+//            statement = connection.prepareStatement("insert into orders(timeofdelivery,"+
+//                    " deliveryaddress, status, dates, usernamesalesman, usernamecustomer, subscriptionid, "+
+//                    "postalcode) values(?, ?, ?, ?, ?, ?, ?, ?)");
+//            statement.setInt();
+//        }
+//    }
+    
 
     public boolean userExist(String username) {
         PreparedStatement sqlLogIn = null;
@@ -263,6 +254,36 @@ public class Database {
         closeConnection();
         return exist;
     }
+    
+    public User getUser(){
+        PreparedStatement statement = null;
+        openConnection();
+        User newUser = new User();
+        try {
+            statement = connection.prepareStatement("SELECT * FROM users WHERE username = '" + currentUser + "'");
+            ResultSet res = statement.executeQuery();
+            connection.commit();
+            while (res.next()) {
+                String username = res.getString("username");
+                String password = res.getString("password");
+                String firstname = res.getString("firstname");
+                String surname = res.getString("surname");
+                String address = res.getString("address");
+                int mobilenr = res.getInt("moblienr");
+                int postalcode = res.getInt("postalcode");
+                newUser = new User(username, password, firstname, surname, address, mobilenr, postalcode);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            Cleaner.rollback(connection);
+
+        } finally {
+            Cleaner.setAutoCommit(connection);
+            Cleaner.closeSentence(statement);
+        }
+        closeConnection();
+        return newUser;
+    }
 
     private void openConnection() {
         try {
@@ -280,94 +301,4 @@ public class Database {
         System.out.println("Closing databaseconnection");
         Cleaner.closeConnection(connection);
     }
-    /*
-     public boolean regNew(user user) {
-     PreparedStatement sqlRegNew = null;
-     openConnection();
-     boolean ok = false;
-     try {
-     sqlRegNew = connection.prepareStatement("insert into trening(dato,varighet,kategorinavn,tekst,brukernavn) values(?, ?, ?,?,?)");
-     try {
-     sqlRegNew.setDate(1, new java.sql.Date(newWorkout.getDate().getTime()));
-     } catch (NullPointerException e) {
-     sqlRegNew.setDate(1, new java.sql.Date(new java.util.Date().getTime()));
-     }
-     sqlRegNew.setInt(2, newWorkout.getDuration());
-     sqlRegNew.setString(3, newWorkout.getCategory());
-     sqlRegNew.setString(4, newWorkout.getText());
-     sqlRegNew.setString(5, user);
-     sqlRegNew.executeUpdate();
-
-     connection.commit();
-
-     ok = true;
-
-     } catch (SQLException e) {
-     System.out.println(e.getMessage());
-     Cleaner.rollback(connection);
-
-     } finally {
-     Cleaner.setAutoCommit(connection);
-     Cleaner.closeSentence(sqlRegNew);
-     }
-     closeConnection();
-     return ok;
-     }
-     */
-    /*
-     public boolean deleteWorkout(user user) {
-     boolean ok = false;
-     PreparedStatement sqlUpdWorkout = null;
-
-     System.out.println(workout.getNumber());
-     openConnection();
-     try {
-     sqlUpdWorkout = connection.prepareStatement("DELETE FROM TRENING WHERE oktnr = ?");
-     sqlUpdWorkout.setInt(1, workout.getNumber());
-     sqlUpdWorkout.executeUpdate();
-     connection.commit();
-     ok = true;
-
-     } catch (SQLException e) {
-     Cleaner.writeMessage(e, "deleteWorkout()");
-     } finally {
-     Cleaner.closeSentence(sqlUpdWorkout);
-     }
-     closeConnection();
-     return ok;
-
-
-
-     }
-
-     public boolean changeData(Workout workout) {
-     PreparedStatement sqlUpdWorkout = null;
-     boolean ok = false;
-     openConnection();
-     try {
-     //String sql = "update eksemplar set laant_av = '" + navn + "' where isbn = '" + isbn + "' and eks_nr = " + eksNr;
-     sqlUpdWorkout = connection.prepareStatement("update trening set dato = ?,varighet = ?,kategorinavn = ?, tekst = ? where oktnr = ? and brukernavn = ?");
-     try {
-     sqlUpdWorkout.setDate(1, new java.sql.Date(workout.getDate().getTime()));
-     } catch (NullPointerException e) {
-     sqlUpdWorkout.setDate(1, new java.sql.Date(new java.util.Date().getTime()));
-     }
-     sqlUpdWorkout.setInt(2, workout.getDuration());
-     sqlUpdWorkout.setString(3, workout.getCategory());
-     sqlUpdWorkout.setString(4, workout.getText());
-     sqlUpdWorkout.setInt(5, workout.getNumber());
-     sqlUpdWorkout.setString(6, user);
-     ok = true;
-     sqlUpdWorkout.executeUpdate();
-     connection.commit();
-     } catch (SQLException e) {
-     Cleaner.writeMessage(e, "changeData()");
-     } finally {
-     Cleaner.closeSentence(sqlUpdWorkout);
-     }
-     closeConnection();
-     return ok;
-     }
-     * 
-     */
 }
